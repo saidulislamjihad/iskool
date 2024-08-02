770da84d6475d81206202b658dff2ec38e3d4440
saidulislamjihad <saidulislamjihad20@gmail.com>
2024-08-02 23:24:38 +0600

    Worked on home page

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,3 +30,74 @@ $('.home-items-wrapper').owlCarousel({
         }
     }
 });
+
+// Courses Slider Js
+$('.course-items-wrapper').owlCarousel({
+    loop: true,
+    margin: 10,
+    dots: false,
+    nav: true,
+    autoplay: 3000,
+    smartSpeed: 1000,
+    responsiveClass:true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 4
+        }
+    }
+});
+
+
+// Testimonial Slider Js
+$('.testimonial-items-wrapper').owlCarousel({
+    loop: true,
+    margin: 10,
+    dots: true,
+    nav: false,
+    autoplay: 3000,
+    smartSpeed: 1000,
+    responsiveClass:true,
+    stagePadding: 350,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+});
+
+//Client Slider JS
+$('.client-items-wrapper').owlCarousel({
+  loop: true,
+  margin: 0,
+  nav: false,
+  responsiveClass: true,
+  autoplay: true,
+  smartSpeed:1000,
+  responsive: {
+    0: {
+      items: 1
+    },
+    676: {
+      items: 2
+    },
+    991: {
+      items: 4
+    },
+    1000: {
+      items: 6
+    }
+  }
+  
+});
